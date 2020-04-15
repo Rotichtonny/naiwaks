c0e66e9209374825d67f582ef32fe70a8b838734
Rotichtonny <tonnyrotich.tr@gmail.com>
2020-04-15 04:05:07 +0300

    Created Template class & libraries

---
+++ templates/includes/footer.php
@@ -0,0 +1,8 @@
+            <footer class="footer">
+                <p>&copy; <?php echo date("Y"); ?> NaiWaks.</p>
+            </footer>
+
+        </div> <!-- /container -->
+    </body>
+
+</html>
---
+++ templates/includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NaiWaks</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header clearfix">
+            <nav>
+                <ul class="nav nav-pills pull-right">
+                    <li role="presentation"><a href="index.php">Home</a></li>
+                    <li role="presentation"><a href="create.php">Create Jobs</a></li>
+                </ul>
+            </nav>
+            <h3 class="text-muted">NaiWaks</h3>
+        </div>
